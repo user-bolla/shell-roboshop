0b0289ac0783632904116fe5490a20fae37b5c89
srimanth bolla <srimanthbolla9126@gmail.com>
2025-10-08 15:08:06 -0400

    session

--- roboshop.sh
+++ roboshop.sh
@@ -2,7 +2,7 @@
 
 AMI_ID="ami-09c813fb71547fc4f"
 SG_ID="sg-09d07490acc07ecdc" # replace with your SG ID
-# ZONE_ID="Z0948150OFPSYTNVYZOY" # replace with your ID
+ZONE_ID="Z09862333DG9EIMMVCYEW" # replace with your ID
 DOMAIN_NAME="userbolla.store"
 
 for instance in $@
@@ -18,4 +18,24 @@ do
             RECORD_NAME="$DOMAIN_NAME" # daws86s.fun
         fi
         echo "$instance: $IP"
+        echo "$instance: $IP"
+
+        aws route53 change-resource-record-sets \
+        --hosted-zone-id $ZONE_ID \
+        --change-batch '
+        {
+            "Comment": "Updating record set"
+            ,"Changes": [{
+            "Action"              : "UPSERT"
+            ,"ResourceRecordSet"  : {
+                "Name"              : "'$RECORD_NAME'"
+                ,"Type"             : "A"
+                ,"TTL"              : 1
+                ,"ResourceRecords"  : [{
+                    "Value"         : "'$IP'"
+                }]
+            }
+            }]
+        }
+        '
 done 
