3b1af360d1228265c7ece609ab59905d7a9ec4d7
srimanth bolla <srimanthbolla9126@gmail.com>
2025-10-08 18:56:26 -0400

    session

--- mysql.sh
+++ mysql.sh
@@ -27,20 +27,15 @@ VALIDATE(){ # functions receive inputs through args just like shell script args
     fi
 }
 
-dnf module disable redis -y &>>$LOG_FILE
-VALIDATE $? "Disabling Default Redis"
-dnf module enable redis:7 -y &>>$LOG_FILE
-VALIDATE $? "Enabling Redis 7"
-dnf install redis -y  &>>$LOG_FILE
-VALIDATE $? "Installing Redis"
+dnf install mysql-server -y &>>$LOG_FILE
+VALIDATE $? "Installing MYSQL Server"
+systemctl enable mysqld &>>$LOG_FILE
+VALIDATE $? "Enabling MYSQL server"
+systemctl start mysqld &>>$LOG_FILE
+VALIDATE $? "Starting MYSQL Server"
 
-sed -i -e 's/127.0.0.1/0.0.0.0/g' -e '/protected-mode/ c protected-mode no' /etc/redis/redis.conf
-VALIDATE $? "Allowing Remote connections to Redis"
-
-systemctl enable redis &>>$LOG_FILE
-VALIDATE $? "Enabling Redis"
-systemctl start redis &>>$LOG_FILE
-VALIDATE $? "Starting Redis"
+mysql_secure_installation --set-root-pass RoboShop@1 &>>$LOG_FILE
+VALIDATE $? "Setting up Root password"
 
 END_TIME=$(date +%s)
 TOTAL_TIME=$(( $END_TIME - $START_TIME ))
