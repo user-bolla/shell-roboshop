b225b745c0ae79024dba7c856815f64b8169bff4
srimanth bolla <srimanthbolla9126@gmail.com>
2025-10-08 15:00:42 -0400

    session

--- roboshop.sh
+++ roboshop.sh
@@ -9,7 +9,10 @@ for instance in $@
 do 
         INSTANCE_ID=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t3.micro --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query 'Instances[0].InstanceId' --output text)
 
-        if [ $instance != "frontend"]; then
+        #if [ $instance != "frontend" ]; then
+        #    IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text)
+        #    RECORD_NAME="$instance.$DOMAIN_NAME" # mongodb.daws86s.fun
+        if [ $instance != "frontend" ]; then
             IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text)
             RECORD_NAME="$instance.$DOMAIN_NAME" # mongodb.daws86s.fun
         else
